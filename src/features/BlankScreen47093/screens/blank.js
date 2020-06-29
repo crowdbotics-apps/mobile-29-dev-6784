618f6e8f2e68f6e1ff7e0de45310f3cf5ab072cc
Crowdbotics <team@crowdbotics.com>
2020-06-29 08:43:50 +0000

    Update components

--- src/features/BlankScreen47093/screens/blank.js
+++ src/features/BlankScreen47093/screens/blank.js
@@ -57,7 +57,37 @@ export class _Blank extends React.Component {
         borderRadius: 0,
         backgroundSize: "auto"
       }}
-    />
+    >
+      <Slider
+        value={50}
+        minimumValue={0}
+        maximumValue={100}
+        step={1}
+        disabled={false}
+        style={{
+          marginLeft: 0,
+          marginRight: 0,
+          marginTop: 5,
+          marginBottom: 5,
+          paddingLeft: 5,
+          paddingRight: 5,
+          paddingTop: 5,
+          paddingBottom: 5,
+          overflow: "visible",
+          textAlign: "left",
+          verticalAlign: "baseline",
+          borderColor: "#000000",
+          borderStyle: "solid",
+          borderWidth: 0,
+          borderLeftWidth: 0,
+          borderRightWidth: 0,
+          borderTopWidth: 0,
+          borderBottomWidth: 0,
+          borderRadius: 0,
+          fontFamily: "System"
+        }}
+      />
+    </View>
   )
 }
 
