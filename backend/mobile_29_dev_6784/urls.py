a277e25dc0df835fdd3504f683c13b4c3ed986ea
Crowdbotics <team@crowdbotics.com>
2020-06-29 12:10:04 +0000

    Update admin title

--- backend/mobile_29_dev_6784/urls.py
+++ backend/mobile_29_dev_6784/urls.py
@@ -33,9 +33,9 @@ urlpatterns = [
     path("home/", include("home.urls")),
 ]
 
-admin.site.site_header = "mobile 290"
-admin.site.site_title = "mobile 290 Admin Portal"
-admin.site.index_title = "mobile 290 Admin"
+admin.site.site_header = "mobile 29080"
+admin.site.site_title = "mobile 29080 Admin Portal"
+admin.site.index_title = "mobile 29080 Admin"
 
 # swagger
 api_info = openapi.Info(
